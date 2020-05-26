f9b5bd9fac4f37a853b7f1fd6a568e1fa926788d
Billy Jacobs <billy@gides.com>
2020-05-25 20:32:18 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,93 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { 
+  Paper, AppBar, Toolbar, IconButton, Typography, Button, makeStyles, createStyles, Theme 
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: theme.spacing(1),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
 
 function App() {
+  const classes = useStyles();
+  const getRandomColor = (value: number) => {
+    const selectorValue = value % 6;
+    switch (selectorValue) {
+      case 0:
+        return 'darkred';
+      case 1:
+        return '#356277';
+      case 2:
+        return '#75cc75';
+      case 3:
+        return 'transparent';
+      case 4:
+        return 'purple';
+      case 5:
+        return 'orange';
+      default:
+        return 'white';
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.root}>
+        <AppBar position="relative" className="headerContainer">
+          <IconButton edge="start" 
+            className={classes.menuButton} 
+            color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            My First App looking Web App
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </AppBar>
+      </div>
+      <Paper className="contentContainer" square={true}>
+        {/* Generate many rows */}
+        <div style={{display: 'flex', flexWrap: 'wrap', flex: 1}}>
+          {[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+            1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+            1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+            1,1,1,1,1,1,1,1,1,1,1,1].map((value, index) =>
+            <div style={{display: 'flex', margin: '5px',
+                  border: 'solid 1px white', 
+                  background: getRandomColor(index),
+                  width: '95px', height: '95px',
+                  borderRadius: '20px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  fontSize: '30px' 
+                }} key={index}>
+              {index + 1}
+            </div>
+          )}
+        </div>
+      </Paper>
+      <AppBar position="relative" className="footerContainer">
+        <IconButton color="inherit">
+          <SearchIcon />
+        </IconButton>
+        <IconButton edge="end" color="inherit">
+          <MoreIcon />
+        </IconButton>
+      </AppBar>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+
+const outerTheme = createMuiTheme({
+  palette: {
+    background: {paper: '#3b4853'},
+    primary: {
+      main: '#8fa7bb',
+    },
+    type: 'dark',
+  },  
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={outerTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
